35e10fae8a4c5b85539113782f195200a05a81c2
ChamlingUttam <uttamchamling51@gmail.com>
2026-03-04 19:22:22 +0545

    pratice

--- 3.js
+++ 3.js
@@ -1,1 +1,25 @@
-//longest common perfix
+//anagram 
+// its like rearrange the word
+const checkAnagram = (str) => {
+
+    let word = 'abcdefg';
+
+    if (str.length !== word.length) {
+        console.log("word is not anagram");
+        return;
+    }
+
+    let sortWord = word.split('').sort();
+    let strWord = str.split('').sort();
+
+    for (let i = 0; i < sortWord.length; i++) {
+        if (sortWord[i] !== strWord[i]) {
+            console.log("word is not anagram");
+            return;
+        }
+    }
+
+    console.log("is anagram");
+}
+
+checkAnagram('abcdfge'); // ✅ is anagram
