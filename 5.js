4f57ddd2b4850830bf25556903748583d244f209
ChamlingUttam <uttamchamling51@gmail.com>
2026-03-05 18:54:25 +0545

    pratice

---
+++ 5.js
@@ -0,0 +1,22 @@
+//common string or char
+
+
+
+
+//reverse pratice
+// const reverseString = (str)=>{
+
+//     let result =""
+
+//     for(let i =str.length -1; i>=0; i--){
+//          result+=str[i]
+//     }
+//     console.log(result)
+// }
+
+// reverseString("qwerty")
+
+
+
+
+
---
+++ 6.js
@@ -0,0 +1,16 @@
+// captilize the every first charcter of word
+
+const capitalFirst = (str)=>{
+
+    let splitWord = str.split(" ")
+   let result = ""
+
+    for(let i = 0; i<splitWord.length; i++){
+        result += splitWord[i]
+
+        
+    }
+
+}
+
+capitalFirst("this is to be capital")
